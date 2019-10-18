22effc7035f53a079d2623912fc057958149f7ce
Alex Pina <alexpina16@msn.com>
2019-10-17 18:51:25 -0700

    controllers updated

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -1,5 +1,38 @@
 knexconst knex = require('../db/')
 
-// exports.getAllAppointments = function (req, res) {
-//     knex('appointments').then(appointments => res.json(appointments))
-// }
+exports.getAllAppointments = function (req, res) {
+    knex('appointments')
+        .then(appointments => res.json(appointments))
+}
+
+exports.getOneAppointment = function (req, res) {
+    knex('appointments')
+        .where('id', req.params.id)
+        .then(appointments => res.json(appointments))
+}   
+
+exports.addOneBook = function (req, res) {
+    knex('appointments')
+        .insert(req.body)
+        .returning('*')
+        .then(newAppointment => res.json(newAppointment))
+}
+
+exports.updateOneAppointment = function (req, res) {
+    knex('appointments') 
+        .update({
+            ...req.body,
+            updated_at: new Date()
+        })
+        .where('id', req.params.id)
+        .returning('*')
+        .then(updatedAppointment => res.json(updatedAppointment))
+}
+
+exports.removeOneAppointment = function (req, res) {
+    knex('appointments')
+        .del()
+        .where('id, req.params.id')
+        .returning('*')
+        .then(newApppointment => res.json(newApppointment))
+}
