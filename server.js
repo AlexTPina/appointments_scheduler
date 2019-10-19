ac53158db03fe5070a187614a7f026b9786a16f1
Alex Pina <alexpina16@msn.com>
2019-10-19 11:19:58 -0700

    updated server.js

--- server.js
+++ server.js
@@ -6,21 +6,18 @@ const cors = require('cors');
 const logger = require('morgan');
 const knex = require('./db/knex');
 
-const index = require('./routes/indexRoutes');
-const todos = require('./routes/todosRoutes');
+const users = require('./routes/users')
+const appointments = require('./routes/appointments')
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', index);
-app.use('/todos', todos);
+app.use('/users', users);
+app.use('/appointments', appointments);
 
 app.listen(port, function() {
   console.log("listening on port: ", port);
